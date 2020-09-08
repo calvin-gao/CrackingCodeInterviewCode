9b4ebf22b49a0c95f926846ce36a21f4e3bfc6fc
Calvin Gao <c_gao3@yahoo.com>
2020-09-08 15:49:43 -0700

    partitionUpdate

--- Ch2LinkedList/partitionList.java
+++ Ch2LinkedList/partitionList.java
@@ -4,6 +4,22 @@ import Ch2LinkedList.removeDup.ListNode;
 
 public class partitionList {
     public ListNode partition(ListNode head, int x) {
-        
+        ListNode smallerHead = new ListNode(0);
+        ListNode biggerHead = new ListNode(0);
+        ListNode smaller = smallerHead;
+        ListNode bigger = biggerHead;
+        while(head != null){
+            if(head.val < x){
+                smaller.next = head;
+                smaller = smaller.next;
+            }else{
+                bigger.next = head;
+                bigger = bigger.next;
+            }
+            head = head.next;
+        }
+        bigger.next = null;
+        smaller.next = biggerHead.next;
+        return smallerHead.next;
     }
 }
