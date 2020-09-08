d56e46af11771ffc562eca8da0aeb98aaf757fdb
Calvin Gao <c_gao3@yahoo.com>
2020-09-07 17:33:22 -0700

    isUnique with driver

--- Ch1ArrayAndStrings/isUnique.java
+++ Ch1ArrayAndStrings/isUnique.java
@@ -1,7 +1,22 @@
 package Ch1ArrayAndStrings;
 
 public class isUnique{
-    public static void main(String [] args){
 
+    public static boolean checkIsUnique(String s){
+        if(s.length() > 128) return false;
+        boolean[] char_set = new boolean[128];
+        for(int i = 0 ; i < s.length() ; i++){
+            int index = s.charAt(i);
+            if(char_set[index]){
+                return false;
+            }
+            char_set[index] = true;
+        }
+        return true;
+    }
+
+    public static void main(String [] args){
+        System.out.println(checkIsUnique("heloplie")); // false
+        System.out.println(checkIsUnique("helo")); // true
     }
 }
